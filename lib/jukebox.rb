d5978f7d4359b33990ededfb76a28681ee7a55fd
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-13 14:38:53 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -18,13 +18,14 @@ end
 def play(songs)
 puts "Please enter a song name or number:"
  input = gets.strip
- songs.each |song| 
+ songs.each do |song| 
     if input == song
       puts "Playing #{song}"
     else
       index = (input.to_i)-1
       puts "Playing #{songs[index]}"
     end 
+  end 
 end
 
 def list(songs)
