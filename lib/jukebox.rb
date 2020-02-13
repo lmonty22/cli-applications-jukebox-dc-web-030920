18d3c5728ec165a401a7c6b118722f143454c95d
Lindsay Montgomery <lindsay.montgomery@olympian.org>
2020-02-13 14:09:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -3,7 +3,17 @@ def say_hellp(name)
   "Hi #{name}!"
 end 
 
+def user_input
+  gets.strip
+end 
+
 def help 
+  puts "I accept the following commands:"
+  puts "- help: displays this help message"
+  puts "- list: displays a list of songs"
+  puts "- play: lets you choose a song to play"
+  puts "- exit: exits this program"
+  puts "- Please enter a command:"
 end 
 
 def play
@@ -15,7 +25,11 @@ end
 def exit_jukebox 
 end 
 
+def commands(input)
+  if input == help
+end 
 def run(songs)
- puts ""
+ puts "Please enter a command:"
+ 
   
 end 
